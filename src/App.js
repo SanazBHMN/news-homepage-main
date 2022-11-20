b492361156a90221a3e3c45ea492c0529e09b871
SanazBHMN <sanazbahmani35@gmail.com>
2022-11-20 16:42:28 +0330

    Build topics section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import Topics from "./components/Topics";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,7 @@ function App() {
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Hero />
       <News />
+      <Topics />
     </div>
   );
 }
---
+++ src/components/Topics.js
@@ -0,0 +1,48 @@
+import retroPcs from "../assets/images/image-retro-pcs.jpg";
+import laptops from "../assets/images/image-top-laptops.jpg";
+import gaming from "../assets/images/image-gaming-growth.jpg";
+
+function Topics() {
+  const topics = [
+    {
+      id: 1,
+      order: "01",
+      title: "Reviving Retro PCs",
+      text: "What happens when old PCs are given modern upgrades?",
+      img: retroPcs,
+    },
+    {
+      id: 2,
+      order: "02",
+      title: "Top 10 Laptops of 2022",
+      text: "Our best picks for various needs and budgets.",
+      img: laptops,
+    },
+    {
+      id: 3,
+      order: "03",
+      title: "The Growth of Gaming",
+      text: "How the pandemic has sparked fresh opportunities.",
+      img: gaming,
+    },
+  ];
+
+  return (
+    <div>
+      {topics.map((topic) => (
+        <div className="flex align-center my-8">
+          <div className="mr-6 w-[250px]">
+            <img src={topic.img} className="w-full" />
+          </div>
+          <div>
+            <span className="text-blue-grayish font-bold">{topic.order}</span>
+            <h3 className="text-blue-dark font-bold">{topic.title}</h3>
+            <p className="text-blue-dark-grayish">{topic.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Topics;
