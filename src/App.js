8efa808dd601f70558d0143bd393ef288210a8da
SanazBHMN <sanazbahmani35@gmail.com>
2022-11-20 16:16:05 +0330

    Build news section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,6 +10,7 @@ function App() {
     <div className="App px-3 py-8">
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Hero />
+      <News />
     </div>
   );
 }
---
+++ src/components/News.js
@@ -0,0 +1,35 @@
+function News() {
+  const news = [
+    {
+      id: 0,
+      title: "Hydrogen VS Electric Cars",
+      text: "Will hydrogen-fueled cars ever catch up to EVs?",
+    },
+    {
+      id: 1,
+      title: "The Downsides of AI Artistry",
+      text: "What are the possible adverse effects of on-demand AI image generation?",
+    },
+    {
+      id: 2,
+      title: "Is VC Funding Drying Up?",
+      text: "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+    },
+  ];
+
+  return (
+    <div className="bg-blue-dark text-white-off p-5 my-14">
+      <h1 className="text-orange-soft font-bold">New</h1>
+      <div className="divide-y divide-blue-dark-grayish ">
+        {news.map((newsItem) => (
+          <div key={newsItem.id} className="py-8 leading-8">
+            <h3 className="font-bold">{newsItem.title}</h3>
+            <p className="text-blue-grayish">{newsItem.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default News;
